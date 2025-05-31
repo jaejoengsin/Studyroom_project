c40e7b1cdd80f4b43b1e6caa9b13821e2d60460b
jaejoengsin <zhzhqkq0226@gmail.com>
2025-06-01 02:00:11 +0900

    마지막 수정

--- assets/scripts/time_theme.js
+++ assets/scripts/time_theme.js
@@ -1,13 +1,15 @@
-  function updateTheme() {
-            const now = new Date();
-            const hour = now.getHours();
-            const isDaytime = hour >= 6 && hour < 18; // 일출 6시부터 일몰 18시까지 낮
-
-            if (isDaytime) {
-              document.body.classList.add('daytime');
-              document.body.classList.remove('nighttime');
-            } else {
-              document.body.classList.add('nighttime');
-              document.body.classList.remove('daytime');
-            }
-          }
+   function updateTheme() {
+      const now = new Date();
+      const hour = now.getHours();
+      const isDaytime = hour >= 6 && hour < 18;
+      const logo = document.getElementById("logo");
+      if (isDaytime) {
+        document.body.classList.add('daytime');
+        document.body.classList.remove('nighttime');
+        logo.src = 'assets/images/icon2.png';
+      } else {
+        document.body.classList.add('nighttime');
+        document.body.classList.remove('daytime');
+        logo.src = 'assets/images/nighticon.png';
+      }
+    }
